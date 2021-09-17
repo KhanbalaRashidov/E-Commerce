e3fac1500548e70921822d0ad816bc9b26544f4d
KhanbalaRashidov <reshidovxanbala@gmail.com>
2021-09-18 02:30:10 +0300

    Create Category Model

---
+++ E-Commerce.WebUI/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using E_Commerce.WebUI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Commerce.WebUI.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
---
+++ E-Commerce.WebUI/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace E_Commerce.WebUI.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+        
+    }
+}
